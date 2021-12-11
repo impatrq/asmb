f6ee38074451b757b593dac06993c7a47dc7d578
Cuccaro-Marco <cuccaromarco13@gmail.com>
2021-12-11 17:14:08 -0300

    Style: Agregar comentarios

--- Codigo de Prueba.py
+++ Codigo de Prueba.py
@@ -39,11 +39,16 @@ setupPines()
     alcol = 100300
     desinf = 5000000
     panel = 500000
+
+#Bucle Principal, cada N-tiempo realizo N-tarea
 while(1):
-
+    #Bucle de tiempo
     TimaMiler = TimaMiler + 1
-    if(TimaMiler == desinf):
 
+    #si estiempo maximo(Accion desinfectante)
+    if(TimaMiler == desinf):
+        
+        #Reset Tiempos
         TimaMiler = 0
         Test = 10050
         proxen = 1000
@@ -51,66 +56,83 @@ while(1):
         alcol = 100300
         desinf = 5000000
         panel = 500000
+
+        #Si se esta dentro
         if(Entro):
-
+            #Activo el desinfectante
             GPIO.output(11, 1)
-            
+            #Cuento un pulso
             a = a + 1
+            #si llego a 3 pulsos
             if(a == 3):
-
+                #Paro el desinfectante y reseteo pulsos
                 a = 0
                 GPIO.output(11, 0)
-                
+        #si salio   
         else:
-
+            #Apago desinfectante y reseteo pulsos
             a = 0
             GPIO.output(11, 0)
 
+    #si es tiempo de leer Proximidad de entrada
     if(TimaMiler == proxen):
-
+        #si Senso la entrada
         if(GPIO.input(24)):
-
+            #Enciendo cartel de ocupado
             GPIO.output(15, 1)
+            #Declaro que "Esta dentro de la cabina"
             entro = 1
-
+        #Declaro proximo tiempo de Lectura
         proxen = proxen + 1000
 
+    #si es tiempo de leer el alcohol
+
     if(TimaMiler == alcol):
-
+        #si pone las manos
         if(GPIO.input(18)):
-
+            # Cuento tiempo de dispenser
             a = a + 1
-            if(a <= 100):
+            #si el tiempo de dispenser es menor al maximo
+            if(a <= 60):
+                #Activo dispenser
                 GPIO.output(10, 1)
-
+ ####        
+        #si no tiene las manos
         if(not GPIO.input(18)):
-
+            #Reseteo el contador
             a = 0
+            #apago el dispenser
             GPIO.output(10, 0)
-
+        #Declao proximo tiempo de lectura
         alcol = alcol + 1000000
 
+    #Si es tiempo de Testear
     if(TimaMiler == Test):
-
+        #si toca el boton de test
         if(GPIO.input(17)):
-
+            #Carga todos los estados en 1
             logSensors(1)
-
+        #Declaro proximo tiempo de testeo
         Test = Test + 10000
+    
+    #si es tiempo de leer la salida
     if(TimaMiler == proxsa):
-
+        #si esta saliendo
         if(GPIO.input(23)):
-
+            #Declaro que esta fuera
             Entro = 0
+            #Apago cartel de ocupado
             GPIO.output(15, 1)
+            #Apago Motor desinfectante(en caso que este activado)
             GPIO.output(11, 0)
-
+        #declaro proximo tiempo de Lectura de salida
         proxsa = proxsa + 10000
 
+    #si es tiempo de actualizar el panel
     if(TimaMiler == panel):
-
+        #si no esta haciendo un test
         if(not GPIO.input(17)):
-
+            #Carga los estados al panel
             logSensors(0)
-
+        #declaro proximo tiempo de actualizacion del panel
         panel = panel + 500000
